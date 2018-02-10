a8cfed1647a3a16bdb8454c82b4c5fdcba278006
Chris Heckler <hecklerchris@hotmail.com>
2018-02-09 20:05:59 -0800

    Stage 1

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -4,7 +4,12 @@
 import json
 
 # Stage 0
-stage_0 = json.dumps(dict([('Chris', 80000),('Freddie', 50000), ('Jami', 10000), ('Heckie', 1000000)]))
-print("Stage 0 data: "+ stage_0)
+stage_0_data = dict([('Chris', 80000),('Freddie', 50000), ('Jami', 10000), ('Heckie', 1000000)])
+stage_0_json = json.dumps(stage_0_data)
+print("Stage 0 data: " + stage_0_json)
 
 
+# Stage 1
+stage_1_data = {k.lower(): v for k,v in stage_0_data.items()}
+stage_1_json = json.dumps(stage_1_data)
+print('Stage 1 data: '+ stage_1_json)
