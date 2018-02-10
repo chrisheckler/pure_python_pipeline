2554377610b8a85e993702ed3af6c5044264b88a
Chris Heckler <hecklerchris@hotmail.com>
2018-02-09 19:44:23 -0800

    Stage 0

---
+++ pipeline/pipeline.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Copyright (c) 2018 Chris Heckler <hecklerchris@hotmail.com>
+
+import json
+
+# Stage 0
+stage_0 = json.dumps(dict([('Chris', 80000),('Freddie', 50000), ('Jami', 10000), ('Heckie', 1000000)]))
+print("Stage 0 data: "+ stage_0)
+
+
